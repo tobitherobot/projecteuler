dec83aa8e7130a62148fabf5a469792315abd590
tobitherobot <baumann.tobias@outlook.com>
2021-12-06 14:54:53 +0100

    solved 12

---
+++ p012.py
@@ -0,0 +1,21 @@
+import math
+
+def count(n):
+    c = 2
+    root = int(math.sqrt(n))
+    for i in range(2, root):
+        if n%i==0:
+            c += 2
+    if root*root==n:
+        c += 1
+    return c
+
+n = 1
+a = 2
+c = count(n)
+
+while c<=500:
+    n += a
+    a += 1
+    c = count(n)
+print(n)
