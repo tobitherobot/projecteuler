4bba7c0fa0b406610f7e34e2042a110700214afa
tobitherobot <baumann.tobias@outlook.com>
2021-12-06 22:04:39 +0100

    solved 33

---
+++ p033.py
@@ -0,0 +1,27 @@
+import math
+
+f = [1, 1]
+for n in range(12, 100):
+    for d in range(n+1, 100):
+        if n%10!=0 and n%11!=0 and d%10!=0 and d%11!=0:
+            if str(n)[0]==str(d)[0]:
+                a = int(str(n)[1])
+                b = int(str(d)[1])
+            elif str(n)[0]==str(d)[1]:
+                a = int(str(n)[1])
+                b = int(str(d)[0])
+            elif str(n)[1]==str(d)[0]:
+                a = int(str(n)[0])
+                b = int(str(d)[1])
+            elif str(n)[1]==str(d)[1]:
+                a = int(str(n)[0])
+                b = int(str(d)[0])
+            else:
+                continue
+            
+            g1 = math.gcd(n, d)
+            g2 = math.gcd(a, b)
+            if n/g1==a/g2 and d/g1==b/g2:
+                f[0] *= a
+                f[1] *= b
+print(f[1] // math.gcd(f[0], f[1]))
