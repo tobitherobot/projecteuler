d54e442cf42dd3c85810baa02e275f20c340616e
tobitherobot <baumann.tobias@outlook.com>
2021-12-28 14:11:21 +0100

    solved p7 hr

--- problems/p007.py
+++ problems/p007.py
@@ -1,13 +1,17 @@
-primes = [2]
-n = 3
+#!/bin/python3
 
-while len(primes)<10001:
-    flag = True
-    for p in primes:
-        if n%p==0:
-            flag = False
-            break
-    if flag:
-        primes.append(n)
-    n += 2
-print(primes[-1])
+import sys
+
+def rwh_primes1(n):
+    sieve = [True] * (n//2)
+    for i in range(3, int(n**0.5) + 1,2):
+        if sieve[i//2]:
+            sieve[i*i//2::i] = [False] * ((n-i*i-1) // (2*i)+1)
+    return [2] + [2*i+1 for i in range(1, n//2) if sieve[i]]
+
+primes = rwh_primes1(104730)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(primes[n-1])
