a311a5e069394a45eaf74e469f60b8b449016b0a
tobitherobot <baumann.tobias@outlook.com>
2021-12-28 04:47:41 +0100

    solved p004 hr

--- problems/p004.py
+++ problems/p004.py
@@ -1,7 +1,27 @@
-prod = 0
-for i in range(900, 1000):
-    for j in range(900, 1000):
-        s = i*j
-        if str(s)==str(s)[::-1]:
-            prod = max(prod, s)
-print(prod)
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+s = set()
+for i in range(100, 1000):
+        for j in range(i, 1000):
+            if 101101 <= i*j and len(str(i*j))==6 and str(i*j)==str(i*j)[::-1]:
+                s.add(i*j)
+l = list(s)
+
+for a0 in range(t):
+    n = int(input().strip())
+    l.append(n)
+    l.sort()
+    res = l[l.index(n)-1]
+    print(res)
+
+''' 
+
+100 <= a <= b < 1000
+101101 <= n <= 906609
+
+-> only 279 different in possible range
+
+'''
