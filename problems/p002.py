4015d7150657de23f176479441852d08941ccb02
tobitherobot <baumann.tobias@outlook.com>
2021-12-28 01:33:01 +0100

    solved p002 hr

--- problems/p002.py
+++ problems/p002.py
@@ -1,10 +1,18 @@
-a = 1
-b = 1
-sum = 0
-while a<4000000:
-    if a%2==0:
-        sum += a
-    c = a+b
-    b = a
-    a = c
-print(sum)
+#!/bin/python3
+
+import sys
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+
+    a = 1
+    b = 1
+    sum = 0
+    while a<n:
+        if a%2==0:
+            sum += a
+        c = a+b
+        b = a
+        a = c
+    print(sum)
