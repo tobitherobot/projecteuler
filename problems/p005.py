e7eee469e6d406cf767e6c1fc6e8816a4ba270fd
tobitherobot <baumann.tobias@outlook.com>
2021-12-28 04:52:20 +0100

    updated p4,5 hr

--- problems/p005.py
+++ problems/p005.py
@@ -1,21 +1,27 @@
-fac = {}
-n = 20
+#!/bin/python3
 
-for i in range(1, n+1):
-    n = i
-    d = 2
-    while n!=1:
-        c = 0
-        while n%d==0:
-            n //= d
-            c += 1
-        if 0<c:
-            if d in fac:
-                fac[d] = max(fac[d], c)
-            else:
-                fac[d] = c
-        d += 1
-prod = 1
-for f in fac:
-    prod *= int(pow(f, fac[f]))
-print(prod)
+import sys
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    fac = {}
+
+    for i in range(1, n+1):
+        n = i
+        d = 2
+        while n!=1:
+            c = 0
+            while n%d==0:
+                n //= d
+                c += 1
+            if 0<c:
+                if d in fac:
+                    fac[d] = max(fac[d], c)
+                else:
+                    fac[d] = c
+            d += 1
+    prod = 1
+    for f in fac:
+        prod *= int(pow(f, fac[f]))
+    print(prod)
