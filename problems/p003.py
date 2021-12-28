9aa167764548e1a780ac5c2c46fb091a21013ddd
tobitherobot <baumann.tobias@outlook.com>
2021-12-28 02:35:53 +0100

    updated p003 hr

--- problems/p003.py
+++ problems/p003.py
@@ -1,8 +1,40 @@
-n = 600851475143
-m = 3
-while 1<n:
-    if n%m==0:
-        n //= m
+#!/bin/python3
+
+import sys
+
+def is_prime(n):
+    if n<3:
+        if n==2:
+            return True
+        else:
+            return False
+    elif n%2==0:
+        return False
+    d = 3
+    while d*d <= n:
+        if n % d == 0:
+            return False
+        d += 2
+    return True
+
+def get_smallest_factor(n):
+    if is_prime(n):
+        return n
+    elif n%2==0:
+        return 2
     else:
-        m += 2
-print(m)
+        m = 3
+        while n%m!=0:
+            m += 2
+        return m
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    
+    f = 1
+    while n > 1:
+        f = get_smallest_factor(n)
+        while n%f == 0:
+            n //= f
+    print(f)
